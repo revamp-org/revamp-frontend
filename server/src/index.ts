5c2a96002452cdbdf9163cca286986bba705ec01
NotSugat <sugatsujakhu@gmail.com>
2023-09-06 16:31:18 +0545

    Connection to db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+import express, { Request, Response } from "express"
+import client from "./db"
+const app = express();
+
+
+// middleware
+app.use(express.json())
+
+
+app.listen(5000, () => {
+	console.log("server listening in port 5000");
+}) 
