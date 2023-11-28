afecb958ddb2ccfa3725ae7a249e19f80fb688b6
Pranish <pranishkafle1@gmail.com>
2023-11-28 13:14:42 +0545

    Initial Setup For landing Page Done

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,25 +1,10 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import Navbar from "./components/navbar";
+import Image from "next/image";
 
 const Landing = () => {
 	return (
-		<>
-			<h1>Landing</h1>
-			<SignedOut>
-				<Link href="/sign-in" className="btn">
-					Sign in
-				</Link>
-				<Link href="/sign-up" className="btn">
-					Sign up
-				</Link>
-			</SignedOut>
-			<SignedIn>
-				<Link href="/dashboard" className="btn">
-					Go To Dashboard
-				</Link>
-				<UserButton afterSignOutUrl="/" />
-			</SignedIn>
-		</>
+		
 	);
 };
 
