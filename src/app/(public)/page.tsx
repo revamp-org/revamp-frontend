aed830bd85a4f8aa29e14eb86794d961a28efbfa
Pranish <pranishkafle1@gmail.com>
2023-11-28 14:06:57 +0545

    Initial Setup For landing Page Done

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,8 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import Navbar from "./components/navbar";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const Landing = () => {
 	return (
