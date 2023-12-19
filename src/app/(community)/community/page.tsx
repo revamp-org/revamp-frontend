68707822fca694a06f5e909a4d93ed9b25603e9e
notsugat <sugatsujakhu@gmail.com>
2023-12-19 17:55:03 +0545

    Done journal card and trending sidebar

---
+++ src/app/(community)/community/page.tsx
@@ -0,0 +1,13 @@
+import JournalPostCard from "@/app/components/community/JournalPostCard";
+import JournalTrendingSidebar from "@/app/components/community/JournalTrendingSidebar";
+
+const Community = () => {
+	return (
+		<div className="flex ">
+			<JournalPostCard />
+			<JournalTrendingSidebar />
+		</div>
+	);
+};
+
+export default Community;
---
+++ src/app/(community)/layout.tsx
@@ -0,0 +1,30 @@
+import "@/app/globals.scss";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import ReduxProvider from "@/lib/providers/ReduxProvider";
+import ApolloClientProvider from "@/lib/providers/ApolloClientProvider";
+import Navbar from "../(public)/components/navbar";
+
+export const metadata: Metadata = {
+	title: "Revamp",
+	description:
+		'Revamp is a goal setting app. Our main theme is "Transcending Physical, Mental and Emotional Health."',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning={true}>
+				<body className="dark   mx-auto max-w-[--screen-max] text-foreground ">
+					<ApolloClientProvider>
+						<link rel="shortcut icon" type="image/x-icon" href="/assets/logo.svg" />
+						<ReduxProvider>
+							<Navbar />
+							{children}
+						</ReduxProvider>
+					</ApolloClientProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
+}
