ec1a01ba720c34e2a4be9992d85a9a0d666d068c
kausik10 <kausikpaudel@gmail.com>
2023-12-25 12:15:35 +0545

    Contact page frontend done.

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Footer from "../components/Footer"
 import React, { ChangeEventHandler } from 'react'
 import { useState } from 'react'
 const Contact = () => {
@@ -21,52 +20,53 @@ const Contact = () => {
 	};
 
 	return (
-		<div className="container mx-auto p-8">
-			<h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
-			<form onSubmit={handleSubmit} className="max-w-md mx-auto">
-				<div className="mb-4">
-					<label className="block text-gray-700 text-sm font-bold mb-2">Name:</label>
-					<input
-						type="text"
-						name="name"
-						value={formData.name}
-						onChange={handleChange}
-						className="w-full px-3 py-2 border rounded shadow appearance-none"
-						required
-					/>
-				</div>
-				<div className="mb-4">
-					<label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-					<input
-						type="email"
-						name="email"
-						value={formData.email}
-						onChange={handleChange}
-						className="w-full px-3 py-2 border rounded shadow appearance-none"
-						required
-					/>
-				</div>
-				<div className="mb-6">
-					<label className="block text-gray-700 text-sm font-bold mb-2">Message:</label>
-					<textarea
-						name="message"
-						value={formData.message}
-						onChange={handleChange}
-						className="w-full px-3 py-2 border rounded shadow appearance-none"
-						rows={5}
-						required
-					></textarea>
-				</div>
-				<div>
-					<button
-						type="submit"
-						className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
-					>
-						Submit
-					</button>
-				</div>
-			</form>
-			<Footer />
+		<div className="h-screen-height flex items-center justify-center ">
+			<div className=" flex-row justify-center items-center container mx-auto p-8">
+				<h2 className="text-6xl text-primary-foreground font-semibold text-center mb-4">Contact Us</h2>
+				<form onSubmit={handleSubmit} className="max-w-md mx-auto">
+					<div className="mb-4">
+						<label className="block text-white text-sm font-bold mb-2">Name:</label>
+						<input
+							type="text"
+							name="name"
+							value={formData.name}
+							onChange={handleChange}
+							className="w-full px-3 py-2 border rounded bg-[#D9D9D9] shadow appearance-none"
+							required
+						/>
+					</div>
+					<div className="mb-4">
+						<label className="block text-white text-sm font-bold mb-2">Email:</label>
+						<input
+							type="email"
+							name="email"
+							value={formData.email}
+							onChange={handleChange}
+							className="w-full px-3 py-2 border bg-[#D9D9D9] rounded shadow appearance-none"
+							required
+						/>
+					</div>
+					<div className="mb-6">
+						<label className="block text-white text-sm font-bold mb-2">Message:</label>
+						<textarea
+							name="message"
+							value={formData.message}
+							onChange={handleChange}
+							className="w-full px-3 py-2 border bg-[#D9D9D9] rounded shadow appearance-none"
+							rows={5}
+							required
+						></textarea>
+					</div>
+					<div>
+						<button
+							type="submit"
+							className="w-full px-4 py-2 bg-secondary text-white rounded hover:bg-[#D0E9E9] hover:text-black focus:outline-none focus:shadow-outline-blue "
+						>
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
