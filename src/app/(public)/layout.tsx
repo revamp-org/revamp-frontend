43b6a9d3617ec1efe34b09d19fb189a5c307ac34
notsugat <sugatsujakhu@gmail.com>
2023-12-26 13:00:27 +0545

    Fixed landing page and build issue

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import "@/app/globals.scss"
+import "@/app/globals.scss";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 export const metadata = {
 	title: "Revamp",
@@ -9,19 +9,15 @@ export const metadata = {
 		'Revamp is a goal setting app. Our main theme is "Transcending Physical, Mental and Emotional Health."',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode, }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<ClerkProvider>
 			<html lang="en">
 				<link rel="shortcut icon" type="image/x-icon" href="/assets/logo.svg" />
-				<body className="dark">
-					<div className="min-h-screen flex flex-col">
-						<Navbar />
-						<main>
-							{children}
-						</main>
-						<Footer />
-					</div>
+				<body className="dark ">
+					<Navbar />
+					<main>{children}</main>
+					<Footer />
 				</body>
 			</html>
 		</ClerkProvider>
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,39 +2,33 @@ import Link from "next/link";
 import Image from "next/image";
 const Landing = () => {
 	return (
-		<>
-			<div className="h-screen-height border border-[#1F3148]">
-				<section className="mx-auto mt-24 max-w-screen-2xl items-center px-4 pb-6 md:px-8 lg:flex">
-					<div className="flex-1 space-y-4 sm:text-center lg:text-left">
-						<h1 className="pt-10 text-6xl font-bold text-white xl:text-9xl ">REVAMP</h1>
-						<h1 className=" text-3xl font-semibold text-white ">
-							<span className="text-red-400">
-								Transcending Physical, Mental and Emotional Health
-							</span>
-						</h1>
-
-						<div className="items-center justify-center space-y-3 pt-20 sm:flex sm:space-x-10 sm:space-y-0 lg:justify-start">
-							<div className=" block rounded-md bg-indigo-600 px-6 py-5 text-center text-white shadow hover:bg-red-700 lg:inline ">
-								<Link href="/sign-in">Get Started</Link>
-							</div>
-							<div className="block rounded-md bg-indigo-600 px-6 py-5 text-center text-white shadow hover:bg-red-700 lg:inline">
-								<Link href="/sign-up">How it Works</Link>
-							</div>
+		<div className="h-screen-height  ">
+			<section className="mx-auto  max-w-screen-2xl items-center px-4 pb-6 md:px-8 lg:flex">
+				<div className="flex-1 space-y-4 sm:text-center lg:text-left">
+					<h1 className="pt-10 text-6xl font-bold text-white xl:text-9xl ">REVAMP</h1>
+					<h1 className=" text-3xl font-semibold text-white ">
+						<span className="text-red-400">Transcending Physical, Mental and Emotional Health</span>
+					</h1>
+					<div className="items-center justify-center space-y-3 pt-20 sm:flex sm:space-x-4 sm:space-y-0 lg:justify-start">
+						<div className=" block rounded-md bg-indigo-600 px-6 py-5 text-center text-white shadow hover:bg-red-700 lg:inline ">
+							<Link href="/sign-in">Get Started</Link>
+						</div>
+						<div className="block rounded-md bg-indigo-600 px-6 py-5 text-center text-white shadow hover:bg-red-700 lg:inline">
+							<Link href="/sign-up">How it Works</Link>
 						</div>
 					</div>
-					<div className="mt-7 flex-1 text-center lg:ml-3 lg:mt-0">
-						<Image
-							height={720}
-							width={720}
-							alt="Image"
-							src="https://i.postimg.cc/HxHyt53c/undraw-heatmap-uyye.png"
-							className="mx-auto w-full sm:w-10/12  lg:w-full"
-						/>
-					</div>
-				</section>
-			</div >
-
-		</>
+				</div>
+				<div className="mt-7 flex-1 text-center lg:ml-3 lg:mt-0">
+					<Image
+						height={720}
+						width={720}
+						alt="Image"
+						src="https://i.postimg.cc/HxHyt53c/undraw-heatmap-uyye.png"
+						className="mx-auto w-full sm:w-10/12  lg:w-full"
+					/>
+				</div>
+			</section>
+		</div>
 	);
 };
 
